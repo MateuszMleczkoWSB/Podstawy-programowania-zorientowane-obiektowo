14b716a208bf755b8634545a0e57d34fe431ea2e
Mateusz Mleczko <mateusz.mleczko1@student.wsb.edu.pl>
2026-05-12 14:22:17 +0200

    add starting program method

--- Ex1.py
+++ Ex1.py
@@ -1,7 +1,22 @@
 # Exercise 1: reminder of the basics of programming
 
+# Starting method
+def start():
+    while True:
+        program = int(input("Select program (1: Simple two-number calculator; 2: Temperature converter (Celsius --> Fahrenheit); 3: Student's grades average): "))
+
+        if program == 1:
+            calc()
+        elif program == 2:
+            tempConverter()
+        elif program == 3:
+            gradesAverage()
+        else:
+            start()
+
 # Simple two-number calculator
 def calc():
+    print("Simple two-number calculator")
     num1 = float(input("Enter first num: "))
     num2 = float(input("Enter second num: "))
     operation = input("Enter operation number: ('+' - plus; '-' - minus'; '*' - multiply; '/' - divide) ")
@@ -29,6 +44,7 @@ def calc():
 
 # Temperature converter (Celsius --> Fahrenheit)
 def tempConverter():
+    print("Temperature converter (Celsius --> Fahrenheit)")
     operation = input("Enter operation: ('c' - If you want to convert Celsius to Fahrenheit; 'f' - If you want to convert Fahrenheit to Celsius): ")
     temp = float(input("Enter temperature to convert: "))
 
@@ -47,6 +63,7 @@ def tempConverter():
 
 # Student's grades average
 def gradesAverage():
+    print("Student's grades average")
     numOfGrades = int(input('Enter number of grades: '))
     sum = 0
 
@@ -63,7 +80,10 @@ def gradesAverage():
     average = sum / numOfGrades
     print(f"Grades average: {average}")
 
-    if average > 3:
+    if average >= 3:
         print('Student passed')
     else:
         print('Student failed')
+
+
+start()
