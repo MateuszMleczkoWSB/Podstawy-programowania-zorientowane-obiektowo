8f8fde8073baaaf6582f3c702654ab2050a1eb4e
Mateusz Mleczko <mateusz.mleczko1@student.wsb.edu.pl>
2026-05-12 13:46:45 +0200

    add temp converter + old code cleanup

--- Ex1.py
+++ Ex1.py
@@ -1,23 +1,22 @@
-# Zadanie 1: przypomnienie podstaw programowania
+# Exercise 1: reminder of the basics of programming
 
-# Prosty kalkulator dwóch liczb
-
+# Simple two-number calculator
 def calc():
     num1 = float(input("Enter first num: "))
     num2 = float(input("Enter second num: "))
-    operation = int(input("Enter operation number: (1: '+' - plus; 2: '-' - minus'; 3: '*' - multiply; 4: '/' - divide) "))
+    operation = input("Enter operation number: ('+' - plus; '-' - minus'; '*' - multiply; '/' - divide) ")
 
-    if (operation == 1):
+    if operation == '+':
         result = num1 + num2
-        print(f"Sum: {result}")
-    elif (operation == 2):
+        print(f"Result: {result}")
+    elif operation == '-':
         result = num1 - num2
         print(f"Result: {result}")
-    elif (operation == 3):
+    elif operation == '*':
         result = num1 * num2
         print(f"Result: {result}")
-    elif (operation == 4):
-        if (num2 == 0):
+    elif operation == '/':
+        if num2 == 0:
             print("You can't divide by zero")
             return
         else:
@@ -25,3 +24,20 @@ def calc():
             print(f"Result: {result}")
     else:
         print("Wrong operation number")
+
+
+# Temperature converter (Celsius --> Fahrenheit)
+def tempConverter():
+    operation = input("Enter operation: ('c' - If you want to convert Celsius to Fahrenheit; 'f' - If you want to convert Fahrenheit to Celsius): ")
+    temp = float(input("Enter temperature to convert: "))
+
+    if operation == 'c':
+        print("Celsius --> Fahrenheit")
+        result = (temp * 1.8) + 32
+        print(f"{temp}℃ = {result}℉")
+    elif operation == 'f':
+        print("Fahrenheit --> Celsius")
+        result = (temp - 32) / 1.8
+        print(f"{temp}℉ = {result}℃")
+    else:
+        print("Entered wrong operation (set 'c' or 'f')")
